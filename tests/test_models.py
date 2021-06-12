999242861664297db19b4e197d01cd9135d4e996
Daniel Furman <djfurman@gmail.com>
2021-06-11 22:05:51 -0400

    Add test for business application collection.

--- tests/test_models.py
+++ tests/test_models.py
@@ -2,17 +2,29 @@ from pydantic import ValidationError
 
 from wmd import models
 
+business_application = {
+    "name": "Well Managed Deployments",
+    "is_active": True,
+    "created_at": "2020-02-01T15:31:24.349857",
+    "maintenance_team": "DEVOPS_WMD",
+}
+
 
 def test_business_application_model():
-    business_application = {
-        "name": "Well Managed Deployments",
-        "is_active": True,
-        "created_at": "2020-02-01T15:31:24.349857",
-        "maintenance_team": "DEVOPS_WMD",
-    }
-
     try:
         models.BusinessApplication(**business_application)
+    except ValidationError as e:
+        print(str(e))
+        assert False
+
+    assert True
+
+
+def test_business_applications_collection_model():
+    try:
+        models.BusinessApplicationsCollection(
+            business_applications=[models.BusinessApplication(**business_application)]
+        )
     except ValidationError as e:
         print(str(e))
         assert False
