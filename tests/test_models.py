b53ba48a7b8baeac515445d97d0135ed9f364e75
Daniel Furman <djfurman@gmail.com>
2021-06-11 21:45:57 -0400

    Add test for pydantic validation of business application objects.

---
+++ tests/test_models.py
@@ -0,0 +1,20 @@
+from pydantic import ValidationError
+
+from wmd import models
+
+
+def test_business_application_model():
+    business_application = {
+        "name": "Well Managed Deployments",
+        "is_active": True,
+        "created_at": "2020-02-01T15:31:24.349857",
+        "maintenance_team": "DEVOPS_WMD",
+    }
+
+    try:
+        models.business_application(**business_application)
+    except ValidationError as e:
+        print(str(e))
+        assert False
+
+    assert True
